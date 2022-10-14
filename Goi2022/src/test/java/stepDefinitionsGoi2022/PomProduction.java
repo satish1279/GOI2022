c1d14b260ce5fc3cb95958c477f6745851b44bd9
SatishJaiswal <SatishJaiswal@DESKTOP-K01PIVP>
2022-10-15 00:29:25 +0530

    logger set at package level to capture info from all the classes

--- Goi2022/src/test/java/stepDefinitionsGoi2022/PomProduction.java
+++ Goi2022/src/test/java/stepDefinitionsGoi2022/PomProduction.java
@@ -25,18 +25,15 @@ import util.TakeSS;
 public class PomProduction {
 
 
-	private static final Logger logger= LogManager.getLogger("PomProduction");
 	static WebDriver driver =null;
 
 
 	PageElements pages;
-
 	TakeSS screenshots;
 
 
 
-
-
+	private static final Logger logger = LogManager.getLogger(PomProduction.class);
 
 	@BeforeStep
 	public void beforeStepHook() throws IOException, InterruptedException {
@@ -53,19 +50,27 @@ public class PomProduction {
 
 
 
-
 	@Given ("^User is on Login page wrt Ind_Production$")
 	public void loginPage() throws IOException, InterruptedException {
+
+
+
+
 
 		WebDriverManager.firefoxdriver().setup();
 
 		driver= new FirefoxDriver();
 
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		
+
+
+
 		driver.navigate().to("https://goinfinity.beyond360tech.com/");
 
+
+		
 		logger.info("Browser Started on Goi- India Production environment");
 		logger.info("This is login page");
 	}
@@ -79,10 +84,10 @@ public class PomProduction {
 		pages.entrUsernam();
 		pages.entrPassword();
 		//pages.clikButon();	
-		
+
 		Thread.sleep(5000);
 		logger.info("This is Homepage page");
-		
+
 	}
 
 
@@ -248,9 +253,9 @@ public class PomProduction {
 
 		System.out.println("this is login page");
 		Thread.sleep(1000);
-		
+
 		driver.close();
-		
+
 		logger.info("Browser Closed");
 
 
--- Goi2022/src/test/java/stepDefinitionsGoi2022/PomStaging.java
+++ Goi2022/src/test/java/stepDefinitionsGoi2022/PomStaging.java
@@ -25,36 +25,43 @@ import util.TakeSS;
 public class PomStaging {
 
 
-	private static final Logger logger= LogManager.getLogger("PomStaging");
 	static WebDriver driver =null;
 
 
 	PageElements pages;
 	TakeSS screenshots;
+		
+	
+
+	private static final Logger logger = LogManager.getLogger(PomStaging.class);
 	
 
 
 
 
-	
 	@BeforeStep
 	public void beforeStepHook() throws IOException, InterruptedException {
 		Thread.sleep(500);
 	}
 
-	
+
 	@AfterStep
 	public void afterStepHook() throws IOException, InterruptedException {
-		
-	screenshots = new TakeSS(driver);
+
+		screenshots = new TakeSS(driver);
 		Thread.sleep(500);
 	}
 
 
 
+
 	@Given ("^User is on Login page wrt Staging$")
 	public void loginPage() throws IOException, InterruptedException {
+
 		
+
+
+
 		WebDriverManager.firefoxdriver().setup();
 
 		driver= new FirefoxDriver();
@@ -62,9 +69,12 @@ public class PomStaging {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+	
+		
 		driver.navigate().to("https://goinfinity.beyond360apps.com/");
-
-		logger.info("Browser Started on Goi- Staging environment");
+		
+		
+		logger.info("Browser   Started  on  Goi- Staging   environment");
 		logger.info("This is login page");
 	}
 
@@ -77,10 +87,10 @@ public class PomStaging {
 		pages.entrUsernam();
 		pages.entrPassword();
 		//pages.clikButon();	
-		
+
 		Thread.sleep(5000);
 		logger.info("This is Homepage page");
-		
+
 	}
 
 
@@ -246,10 +256,10 @@ public class PomStaging {
 
 		System.out.println("this is login page");
 		Thread.sleep(1000);
-		
+
 		driver.close();
 		logger.info("Browser Closed");
-	
+
 
 	}
 
